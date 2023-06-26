69cfadd8b936e730e1b42aa714c17cc329c2ab8c
chris <c.langer@orbitgrowth.de>
2023-06-26 20:24:05 +0200

    refactor: remove deprecated components (@common)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,20 +2,8 @@
 export * from './Button';
 export { default as Button } from './Button';
 // -----------------------------------------------------
-export * from './CTA';
-export { default as CTA } from './CTA';
-// -----------------------------------------------------
-export * from './CookieBanner';
-export { default as CookieBanner } from './CookieBanner';
-// -----------------------------------------------------
 export * from './Logo';
 export { default as Logo } from './Logo';
-// -----------------------------------------------------
-export * from './PageHeader';
-export { default as PageHeader } from './PageHeader';
-// -----------------------------------------------------
-export * from './Section';
-export { default as Section } from './Section';
 // -----------------------------------------------------
 export * from './Typography';
 export { default as Typography } from './Typography';
